51de66af96be6c6c05bc76e2ebee0bd36e6e1f85
via-shcherba <via.shcherba@gmail.com>
2019-03-13 12:19:43 +0300

    feat: add implementation getZerosCount function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+	let sumzero;
+	/* get max divider */
+	let max = 2;	
+	for(let i = 2; i<= base;i++){
+		if(base % i == 0){
+			while(base % i == 0){
+				base /= i;
+				if(i>max) max = i;
+			}									
+		}		
+	}
+	/* get sum zeros */
+	sumzero = Math.floor(number / max);
+	let rate = max;
+	while(rate < number){
+		rate *= max;
+		if(rate <= number){
+			sumzero += Math.floor(number / rate);
+		}		
+	}	
+	return sumzero;
 }
